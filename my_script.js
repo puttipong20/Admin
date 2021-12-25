119e226b5929cfd633f1c44f6729b356b2cedb9d
unknown <panompari11@gmail.com>
2021-12-25 17:44:17 +0700

    first commit

---
+++ my_script.js
@@ -0,0 +1,21 @@
+// card
+var detail = new Vue({
+    el:'#main',
+    data:{
+        name: "Puttipong Panomprai",
+        order: 2,
+        neworder:"การบ้าน",
+        bounce: 2.71,
+        bouncerate:"GPA",
+        register:4,
+        user : "My good friend",
+        unique:3 + " ชม. / วัน",
+        uniqueN : "เวลาว่าง",
+        expenses:"การใช้จ่าย",
+        // map
+        visitors:"แผนที่",
+        // GPA
+        GPA:"เกรดเฉลี่ยทุกเทอม"
+        
+    }
+})
